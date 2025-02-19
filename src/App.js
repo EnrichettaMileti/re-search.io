fb5031612b18a2951a0f0ad74495a2020d64b1c0
EnrichettaM <mileti.enrichetta@gmail.com>
2025-02-19 13:36:26 +0100

    Set up React app and GitHub Actions

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './research-1.pdf';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to <strong>Your Business Name</strong></h1>
+        <p>We offer great services to help you grow.</p>
+      </header>
+    </div>
+  );
+}
+export default App;
---
+++ src/App.old.js
@@ -0,0 +1,35 @@
+import logo from './research-1.pdf';
+import './App.css';
+
+#function App() {
+#  return (
+#    <div className="App">
+#      <header className="App-header">
+#        <img src={logo} className="App-logo" alt="logo" />
+#        <p>
+#          Edit <code>src/App.js</code> and save to reload.
+#        </p>
+#        <a
+#          className="App-link"
+#          href="https://reactjs.org"
+#          target="_blank"
+#          rel="noopener noreferrer"
+#        >
+#          Learn React
+#        </a>
+#      </header>
+#    </div>
+#  );
+#}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to <strong>Your Business Name</strong></h1>
+        <p>We offer great services to help you grow.</p>
+      </header>
+    </div>
+  );
+}
+export default App;
